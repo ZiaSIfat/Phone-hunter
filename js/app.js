7084077306ac54848d45e49917c7775453543b73
ZiaSifat <ziasifat150@gmail.com>
2022-03-02 01:50:03 +0600

    error message

--- js/app.js
+++ js/app.js
@@ -1,22 +1,31 @@
 const main = document.getElementById('main-div');
 const loadData = () => {
-    const inputValue = document.getElementById('input-field').value;
+    const input = document.getElementById('input-field');
+    const inputValue = input.value;
     const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`
+    input.value = '';
     main.innerHTML = '';
     fetch(url)
         .then(res => res.json())
-        .then(data => displayData(data.data.slice(0, 20)))
+        .then(data => displayData(data.data.slice(0, 20)));
+
 };
 
 const displayData = (phones) => {
 
+    if (phones.length === 0) {
+        const error = document.getElementById('error');
+        error.innerText = 'NO RESULT FOUND'
+    }
     for (const phone of phones) {
         // console.log(phone)
         const div = document.createElement('div');
         div.classList.add('col-lg-4');
+        div.classList.add('col-sm-12');
+        document.getElementById('error').style.dsiplay = 'none';
         div.innerHTML = `
-        <div class="card style="width: 18rem;"">
-        <img src="${phone.image}" class="w-50 p-5 card-img-top" alt="...">
+        <div class="card my-3  text-center" style="width: 18rem;">
+        <img src="${phone.image}" class="w-100 p-5 card-img-top" alt="...">
         <div class="card-body">
             <h3 class="card-title">${phone.brand}</h3>
             <h5>${phone.phone_name}</h5>
@@ -24,6 +33,7 @@ const displayData = (phones) => {
         </div>
         `
         main.appendChild(div);
+        error.innerText = '';
     }
 }
 
@@ -40,17 +50,27 @@ const dislayPhoneDetail = phone => {
     console.log(phone);
     const phoneDetails = document.getElementById('phone-details');
     const div = document.createElement('div')
+    phoneDetails.innerHTML = '';
     div.innerHTML = `
     <div class="card my-5 mx-auto text-center" style="width: 18rem;">
     <img src="${phone.image}" class="card-img-top p-3" alt="...">
     <div class="card-body">
         <h2 class="card-title">${phone.brand}</h2>
         <h5>${phone.name}</h5>
-        <p class="card-text">${phone.releaseDate}</p>
+        <p class="card-text">Release Date: ${phone.releaseDate ? phone.releaseDate : 'Not Found'}</p>
+        <h2 >Main Features</h2>
+        <p class="card-text">Chipset: ${phone.mainFeatures.chipSet}</p>
+        <p class="card-text">Memory: ${phone.mainFeatures.memory}</p>
         <p class="card-text">Storage: ${phone.mainFeatures.storage}</p>
         <p class="card-text">Displaysize: ${phone.mainFeatures.displaySize}</p>
+        <h2>Others Information</h2>
+        <p class="card-text">Bluetooth: ${phone.others.Bluetooth ? phone.others.Bluetooth : ''}</p>
+        <p class="card-text">GPS: ${phone.others.GPS ? phone.others.GPS : ''}</p>
+        <p class="card-text">NFC: ${phone.others.NFC ? phone.others.NFC : ''}</p>
+        <p class="card-text">Radio: ${phone.others.Radio ? phone.others.Radio : ''}</p>
+        <p class="card-text">USB: ${phone.others.USB ? phone.others.USB : ''}</p>
+        <p class="card-text">WLAN: ${phone.others.WLAN ? phone.others.WLAN : ''}</p>
         <p class="card-text">Sensors: ${phone.mainFeatures.sensors}</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
 </div>
     `
